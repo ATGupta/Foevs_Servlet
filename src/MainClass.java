7db28831287ad4f41425b88afe4cee6961a98f12
Arya Tanmay Gupta <arya.tanmay.gupta@gmail.com>
2018-03-18 09:53:21 +0530

    Add files via upload

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -17,9 +17,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/foevs")
 public class MainClass extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private String parentDir;
-	
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -27,44 +27,46 @@ public class MainClass extends HttpServlet {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		PrintWriter sw = response.getWriter();
 		// pw.append("Served at: ").append(request.getContextPath());
 
 		ServletContext context = getServletContext();
 		parentDir = context.getRealPath("Resources_FOEVS_ATG");
-		
-		String un=request.getParameter("un");
-		String ch=request.getParameter("ch");
-		
-		if(un==null)
-			createWebpage("data",sw);
-		else if(un.equalsIgnoreCase("acknowledgement"))
-			createWebpage("acknowledgement/ack",sw);
-		else if(un!=null && ch!=null)
-			createWebpage(un+"/"+ch+"/data",sw);
+
+		String un = request.getParameter("un");
+		String ch = request.getParameter("ch");
+
+		if (un == null)
+			createWebpage("data", sw);
+		else if (un.equalsIgnoreCase("acknowledgement"))
+			createWebpage("acknowledgement/ack", sw);
+		else if (un != null && ch != null)
+			createWebpage(un + "/" + ch + "/data", sw);
 		else
-			createWebpage("data",sw);
+			createWebpage("data", sw);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
-	private void createWebpage(String path, PrintWriter sw) throws IOException{
+
+	private void createWebpage(String path, PrintWriter sw) throws IOException {
 		printData(readFun("page_top"), sw, path);
 		printData(readFun(path), sw, path);
 		printData(readFun("page_bottom"), sw, path);
@@ -77,69 +79,122 @@ public class MainClass extends HttpServlet {
 	}
 
 	private void printData(BufferedReader br, PrintWriter sw, String path) throws IOException {
-		path=path.substring(0, path.length()-4);//remove "data" file
+		path = path.substring(0, path.length() - 4);// remove "data" file
 		for (;;) {
 			String st = br.readLine();
 			if (st == null)
 				break;
-			
-			if(st.startsWith("\\takefrom")) {
+
+			if (st.startsWith("\\takefrom")) {
 				takeFrom(st, sw);
 				continue;
-			}
-			else if(st.startsWith("\\image")) {
-				st=st.substring(7);
-				st= "Resources_FOEVS_ATG/" + path + st;//path already contains "/" at the end
-				sw.println("<p style=\"text-align: center\"><img style=\"height: 300px\" src=\""
-						+st
-						+"\">");
+			} else if (st.startsWith("\\image")) {
+				st = st.substring(7);
+				st = "Resources_FOEVS_ATG/" + path + st;// path already contains "/" at the end
+				sw.println("<p style=\"text-align: center\"><img style=\"height: 300px\" src=\"" + st + "\">");
 				continue;
 			}
 			sw.println(st);
 		}
 	}
-	
-	private void takeFrom(String st, PrintWriter sw) throws IOException{
-		st=st.substring(9);
-		if(st.equals("chapterMenu")) {
-			takeFromChapterMenu(st,sw);
+
+	private void takeFrom(String st, PrintWriter sw) throws IOException {
+		st = st.substring(9);
+		if (st.equals("chapterMenu")) {
+			takeFromChapterMenu(st, sw);
 		}
 	}
-	
-	private void takeFromChapterMenu(String st, PrintWriter sw) throws IOException{
-		BufferedReader br=readFun(st);
-		int tab=-1;
-		int row=0;
-		String unit="";
-		for(;;) {
-			String a=br.readLine();
-			
-			if(a==null) {
-				sw.println("</ul>\n</li>\n</ul>");
+
+	int row = 1;
+	private void takeFromChapterMenu(String st, PrintWriter sw) throws IOException {
+		BufferedReader br = readFun(st);
+		int tab = -1;
+		String unit = "";
+
+		Vector<String> chapters = new Vector<String>();
+
+		for (;;) {
+			String a = br.readLine();
+
+			if (a == null) {
+				if (tab == 2)
+					writeChapters(sw, chapters);
+
+				sw.println("</tbody>");
+				sw.println("</table>");
+				sw.println("<p><marquee><i><b>Click on the name of a chaper to continue.</i></b></marquee>");
+				sw.println("<p>");
 				break;
 			}
-			//a=a.toUpperCase();
-			
-			if(a.startsWith("\t")) {
-				a=a.substring(1);
-				if(tab==0) {
-					sw.println("<ul>");
-					tab=1;
+
+			if (a.startsWith("\t\t")) {
+				a = a.substring(2);
+				chapters.add(a);
+				tab = 2;
+			} else if (a.startsWith("\t")) {
+				a = a.substring(1);
+
+				if (tab == 2)
+					writeChapters(sw, chapters);
+				
+				tab = 1;
+				sw.println("<tr>");
+				sw.println("<td>"+row++);
+				sw.println("<td rowspan=");
+				chapters.add(a);
+			} else {
+				if (tab == -1) {
+					sw.println("<p><marquee><i><b>Click on the name of a chaper to continue.</i></b></marquee>");
+					sw.println("<p>");
+					sw.println("<h2 style=\"text-align: center\">" + a + "</h2>");
+					sw.println(
+							"<table border=1 style=\"margin-right: 100px;margin-left: 100px;border-collapse:collapse\">\n"
+									+ "	<tbody>\n"
+									+ "		<tr>\n"
+									+ "			<th>S. NO.\n"
+									+ "			<th>UNIT\n"
+									+ "			<th>CHAPTER");
+				} else {
+					if (tab == 2) {
+						writeChapters(sw, chapters);
+					}
+					
+					sw.println("\t</tbody>" + "</table>\n");
+					sw.println("<p><marquee><i><b>Click on the name of a chaper to continue.</i></b></marquee>");
+					sw.println("<p>");
+					sw.println("<h2 style=\"text-align: center\">" + a + "</h2>");
+					sw.println(
+							"<table border=1 style=\"margin-right: 100px;margin-left: 100px;border-collapse:collapse\">\n"
+									+ "	<tbody>\n" 
+									+ "		<tr>\n"
+									+ "			<th>S. NO.\n"
+									+ "			<th>UNIT\n"
+									+ "			<th>CHAPTER");
 				}
-				sw.println("<li><a href=\"?un="+unit.replaceAll("\\s", "")+"&ch="+a.replaceAll("\\s", "")+"\">"+a+"</a></li>");
-			}
-			else {
-				unit=a;
-				if(tab==-1) {
-					sw.println("<ul>");
-					tab=0;
-				}
-				else if(tab==1) {
-					sw.println("</ul>\n</li>");
-					tab=0;
-				}
-				sw.println("<li><a href=\"\">"+a+" &#9656</a>");
+				tab = 0;
+				row=1;
 			}
 		}
+	}
+
+	private void writeChapters(PrintWriter sw, Vector<String>chapters) {
+		sw.print(chapters.size() - 1 + ">");
+		sw.println(chapters.get(0));
+		String unit = chapters.get(0).replaceAll(" ", "");
+		sw.print("<td>");
+		sw.println("<a href=\"?un=" + unit + "&ch="
+				+ chapters.get(1).replaceAll(" ", "") + "\">"
+				+ chapters.get(1)
+				+ "</a>");
+
+		for (int i = 2; i < chapters.size(); i++) {
+			sw.println("<tr>");
+			sw.println("<td>" + (row++));
+			sw.println("<td>" + "<a href=\"?un=" + unit + "&ch="
+					+ chapters.get(i).replaceAll(" ", "") + "\">"
+					+ chapters.get(i)
+					+ "</a>");
+		}
+		chapters.removeAllElements();
 	}
 }
